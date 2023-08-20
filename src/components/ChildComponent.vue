5f2572d33e2bc4b78b3cedf7bdd21afb36133285
sultanul kaisar <sultanulkaisar@gmail.com>
2023-08-20 12:15:46 +0600

    Assignment complete

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="child-component">
+      <h2>Welcome to the Child Component</h2>
+      <button @click="emitCustomMessage">Emit Custom Message</button>
+    </div>
+  </template>
+  
+  <script>
+  import { defineEmits } from 'vue';
+  
+  export default {
+    emits: ['messageEmitted'],
+    setup(props, { emit }) {
+      const emitCustomMessage = () => {
+        const customMessage = 'Hello this message from Child Component!';
+        emit('messageEmitted', customMessage);
+      };
+  
+      return {
+        emitCustomMessage,
+      };
+    },
+  };
+  </script>
+
+<style scoped>
+    .child-component {
+  background-color: #ffffff;
+  padding: 20px;
+  margin-top: 20px;
+}
+
+.child-component h2 {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.child-component p {
+  margin-bottom: 10px;
+}
+
+.child-component button {
+  background-color: #3490dc;
+  color: #ffffff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
+  
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="parent-component">
+        <h2>Welcome to the Parent Component</h2>
+        <ChildComponent :message="message" @messageEmitted="handleMessage" />
+        <p class="received-message">Message from Child Component:  {{ receivedMessage }}</p>
+    </div>
+</template>
+
+<script>
+import { defineProps, ref } from 'vue';
+import ChildComponent from './ChildComponent.vue';
+
+export default {
+    components: {
+        ChildComponent,
+    },
+    setup() {
+        const receivedMessage = ref('');
+
+        const handleMessage = (message) => {
+            receivedMessage.value = message;
+        };
+
+        return {            
+            receivedMessage,
+            handleMessage,
+        };
+    },
+};
+</script>
+
+<style scoped>
+.parent-component {
+    background-color: #f2f2f2;
+    padding: 20px;
+}
+
+.parent-component h2 {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 10px;
+}
+
+.parent-component p {
+    margin-bottom: 10px;
+}
+
+.parent-component .received-message {
+  background-color: #5cd7df;
+  padding: 10px;
+  border-radius: 4px;
+}
+</style>
